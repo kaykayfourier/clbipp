97cd2aba0acb4ee37e3d6333fe6f35da9efaa4f7
OkayWheresAamir <hashmi.aamir@outlook.com>
2026-06-30 23:04:20 +0530

    file restructuring

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,4 @@
+// TODO: render <BottomTabBar /> here once Person C ships components/layout/bottom-tab-bar.tsx
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
